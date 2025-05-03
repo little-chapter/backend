d7e3ea09cc2b637744b750b371dbc76a4fb886a8
poyi <poyi10925@gmail.com>
2025-05-04 01:40:56 +0800

    refactor: 管理者取得訂單列表API

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { dataSource } = require("../db/data-source");
 const logger = require('../utils/logger')('Admin');
-const {isNotValidString, isNotValidInteger} = require("../utils/validUtils")
+const {isNotValidString, isNotValidInteger, isValidDateStr, isValidEmail} = require("../utils/validUtils")
 const {verifyToken, verifyAdmin} = require("../middlewares/auth")
 
+function formatDateToYYYYMMDD(dateString){
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // 月份從 0 開始，所以要 + 1，並補零
+    const day = String(date.getDate()).padStart(2, "0"); // 補零
+    return `${year}-${month}-${day}`;
+}
 router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
     try{
         const allowedFilters = {
@@ -20,18 +27,20 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
             sortBy: "string",
             sortOrder: "string",
         }
+        const allowedOrderStatus = ["created", "pending", "shipped", "completed", "cancelled"];
+        const allowedPaymentStatus = ["unpaid", "paid", "refunded"];
+        const allowedShippingStatus = ["notReceived", "processing", "inTransit", "delivered", "returned"];
+        const allowedSortBy = {
+            createdAt: "created_at",
+            finalAmount: "final_amount",
+        }
+        const allowedSortOrder = ["DESC", "ASC"]
         const filters = req.query;
-        console.log(new Date(filters.startDate).toISOString()) //new Date()會轉UTC 要.toISOString()回來比較
-        // const { selectedDate } = req.body; // e.g. '2025-05-02'
-        // ✅ 轉換為 Date 物件（選擇性，可直接用字串也行）
-        // const dateObj = new Date(selectedDate); // 注意：這會自動轉為 UTC 時區
-        // ✅ 把 Date 轉成 YYYY-MM-DD 格式，避免時區誤差
-        // const dateStr = dateObj.toISOString().split('T')[0]; // '2025-05-02'
         for(const key of Object.keys(filters)){
             if(!(key in allowedFilters)){
                 res.status(400).json({
                     status: false,
-                    message: `不支援的搜尋條件：${key}`,
+                    message: "不支援的搜尋條件",
                 })
                 return
             }
@@ -41,7 +50,7 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
                 if(!value || isNotValidInteger(Number(value)) || Number.isNaN(Number(value))){
                     res.status(400).json({
                         status: false,
-                        message: `欄位 ${key} 資料格式不符`,
+                        message: "欄位資料格式不符",
                     })
                     return
                 }
@@ -50,13 +59,62 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
                 if(!value || isNotValidString(value)){
                     res.status(400).json({
                         status: false,
-                        message: `欄位 ${key} 資料格式不符`,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                //日期格式驗證
+                if((key === "startDate" || key === "endDate") && !isValidDateStr(value)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                if(key === "orderStatus" && !allowedOrderStatus.includes(value)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                if(key === "paymentStatus" && !allowedPaymentStatus.includes(value)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                if(key === "shippingStatus" && !allowedShippingStatus.includes(value)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                if(key === "userEmail" && !isValidEmail(value)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
+                    })
+                    return
+                }
+                if(key === "sortBy" && !(value in allowedSortBy)){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符"
+                    })
+                    return
+                }
+                if(key === "sortOrder" && !(allowedSortOrder.includes(value.toUpperCase()))){
+                    res.status(400).json({
+                        status: false,
+                        message: "欄位資料格式不符",
                     })
                     return
                 }
             }
         }
-        //撈取資料
         let orderQuery = dataSource.getRepository("Orders")
             .createQueryBuilder("orders")
             .innerJoin("orders.User", "user")
@@ -69,63 +127,46 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
                 "orders.shipping_status AS shipping_status",
                 "orders.created_at AS created_at",
             ])
-        // '2021-01-01'
-        //有起始無結束 無起始有結束
-        // if(filters.startDate){}
-        // if(filters.endDate){}
-        const allowedOrderStatus = ["created", "pending", "shipped", "completed", "cancelled"];
-        const allowedPaymentStatus = ["unpaid", "paid", "refunded"];
-        const allowedShippingStatus = ["notReceived", "processing", "inTransit", "delivered", "returned"];
-        //訂單狀態
-        if(filters.orderStatus){
-            if(!allowedOrderStatus.includes(filters.orderStatus)){
+        //起始日期 結束日期
+        if(filters.startDate && filters.endDate){
+            if(filters.startDate > filters.endDate){
                 res.status(400).json({
                     status: false,
-                    message: `orderStatus 不支援的搜尋條件：${filters.orderStatus}`,
-                });
-                return
-            }
-            orderQuery = orderQuery
-                .andWhere("orders.order_status =:orderStatus", {orderStatus: filters.orderStatus})
-        }
-        //付款狀態
-        if(filters.paymentStatus){
-            if(!allowedPaymentStatus.includes(filters.paymentStatus)){
-                res.status(400).json({
-                    status: false,
-                    message: `paymentStatus 不支援的搜尋條件：${filters.paymentStatus}`,
-                });
-                return
-            }
-            orderQuery = orderQuery
-                .andWhere("orders.payment_status =:paymentStatus", {paymentStatus: filters.paymentStatus})
-        }
-        //運送狀態
-        if(filters.shippingStatus){
-            if(!allowedShippingStatus.includes(filters.shippingStatus)){
-                res.status(400).json({
-                    status: false,
-                    message: `shippingStatus 不支援的搜尋條件：${filters.shippingStatus}`,
-                });
-                return
-            }
-            orderQuery = orderQuery
-                .andWhere("orders.shipping_status =:shippingStatus", {shippingStatus: filters.shippingStatus})
-        }
-        //使用者信箱
-        if(filters.userEmail){
-            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-            if(!emailRegex.test(filters.userEmail)){
-                res.status(400).json({
-                    status: false,
-                    message: `欄位 userEmail 資料格式不符`,
+                    message: "欄位資料格式不符"
                 })
                 return
             }
+            const start = new Date(filters.startDate + "T00:00:00.000")
+            const end = new Date(filters.endDate + "T23:59:59.999")
+            orderQuery = orderQuery
+                .andWhere("orders.created_at BETWEEN :start AND :end", {start: start, end: end})
+        }
+        if(filters.startDate && !filters.endDate){
+            const start = new Date(filters.startDate + "T00:00:00.000")
+            orderQuery = orderQuery
+                .andWhere("orders.created_at >=:start", {start: start})
+        }
+        if(!filters.startDate && filters.endDate){
+            const end = new Date(filters.endDate + "T23:59:59.999")
+            orderQuery = orderQuery
+                .andWhere("orders.created_at <=:end", {end: end})
+        }
+        if(filters.orderStatus){
+            orderQuery = orderQuery
+                .andWhere("orders.order_status =:orderStatus", {orderStatus: filters.orderStatus})
+        }
+        if(filters.paymentStatus){
+            orderQuery = orderQuery
+                .andWhere("orders.payment_status =:paymentStatus", {paymentStatus: filters.paymentStatus})
+        }
+        if(filters.shippingStatus){
+            orderQuery = orderQuery
+                .andWhere("orders.shipping_status =:shippingStatus", {shippingStatus: filters.shippingStatus})
+        }
+        if(filters.userEmail){
             orderQuery = orderQuery
                 .andWhere("user.email =:Email", {Email: filters.userEmail})
         }
-        //訂單編號
         if(filters.orderNumber){
             orderQuery = orderQuery
                 .andWhere("orders.order_number =:orderNumber", {orderNumber: filters.orderNumber})
@@ -133,44 +174,11 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
         //排序依據
         let sortBy = "created_at";
         let sortOrder = "DESC";
-        const allowedSortOrder = {
-            createdAt: "created_at",
-            finalAmount: "final_amount",
-        }
         if(filters.sortBy){
-            const sortByStr = String(filters.sortBy);
-            if(!(sortByStr in allowedSortOrder)){
-                res.status(400).json({
-                    status: false,
-                    message: `sortBy 不支援的搜尋條件： ${sortByStr}`
-                })
-                return
-            }
-            sortBy = allowedSortOrder[sortByStr]
-            //升降序
-            if(filters.sortOrder){
-                const order = String(filters.sortOrder).toUpperCase();
-                if(!(order === "DESC" || order === "ASC")){
-                    res.status(400).json({
-                        status: false,
-                        message: `欄位 sortOrder 資料格式不符`
-                    })
-                    return
-                }
-                sortOrder = order;
-            }
-        }else{
-            if(filters.sortOrder){
-                const order = String(filters.sortOrder).toUpperCase();
-                if(!(order === "DESC" || order === "ASC")){
-                    res.status(400).json({
-                        status: false,
-                        message: "欄位 sortOrder 資料格式不符"
-                    })
-                    return
-                }
-                sortOrder = order;
-            }
+            sortBy = allowedSortBy[filters.sortBy]
+        }
+        if(filters.sortOrder){
+            sortOrder = filters.sortOrder.toUpperCase();
         }
         orderQuery = orderQuery.orderBy(`orders.${sortBy}`, sortOrder)
         //總筆數
@@ -191,7 +199,7 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
         }
         const skip = (page - 1) * limit;
         const ordersData = await orderQuery
-            .skip(skip)
+            .offset(skip)
             .limit(limit)
             .getRawMany()
         const ordersResult = ordersData.map(order =>{
@@ -202,7 +210,7 @@ router.get("/orders", verifyToken, verifyAdmin, async(req, res, next) =>{
                 orderStatus: order.order_status,
                 paymentStatus: order.payment_status,
                 shippingStatus: order.shipping_status,
-                createdAt: order.created_at
+                createdAt: formatDateToYYYYMMDD(order.created_at)
             }
         })
         res.status(200).json({
