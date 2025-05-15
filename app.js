d1159e8911524cce946469917a2d64858954e77a
poyi <poyi10925@gmail.com>
2025-05-15 23:14:30 +0800

    feat: 新增結帳付款API

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const productsRouter = require("./routes/products");
 const ordersRouter = require("./routes/orders");
 const homepageRouter = require("./routes/homepage");
 const adminRouter = require("./routes/admin");
+const checkoutRouter = require("./routes/checkout");
 
 const app = express();
 app.use(cors());
@@ -36,6 +37,7 @@ app.use("/api/products", productsRouter);
 app.use("/api/orders", ordersRouter);
 app.use("/api/homepage", homepageRouter);
 app.use("/api/admin", adminRouter);
+app.use("/api/checkout", checkoutRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({
