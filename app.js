d230bd9d5cd7108b34323ae33210bf339bc3b66a
yutzu1370 <doim1370523@gmail.com>
2025-05-29 22:08:34 +0800

    1.完成使用者圖像上傳功能 2.完成商品照片上傳功能 3.新增multer套件 4.GCP設定

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const pinoHttp = require("pino-http");
 const logger = require("./utils/logger")("App");
 const usersRouter = require("./routes/users");
 const productsRouter = require("./routes/products");
+const uploadUserAvatarRouter = require("./routes/uploadAvatar");
+const uploadProductImagesRouter = require("./routes/uploadProduct");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(
   pinoHttp({
     logger,
@@ -30,6 +30,8 @@ app.get("/healthcheck", (req, res) => {
 });
 app.use("/api/users", usersRouter);
 app.use("/api/products", productsRouter);
+app.use("/api/upload/avatar", uploadUserAvatarRouter);
+app.use("/api/admin/products", uploadProductImagesRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({
